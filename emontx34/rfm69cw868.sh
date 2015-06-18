d1d3aa0d705817d49830ccbb74208adf4b1fd099
Glyn Hudson <glyn.hudson@gmail.com>
2015-06-18 14:55:34 +0100

    Update rfm69cw868.sh

--- emontx34/rfm69cw868.sh
+++ emontx34/rfm69cw868.sh
@@ -1,3 +1,3 @@
 echo "starting avrdude upload emonTx V3.4 RFM69CW 868Mhz"
-avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTxFirmware/emonTxV3/RFM/emonTxV3.4/emonTxV3_4_DiscreteSampling/emonTxV3_latest_868.hex
+avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTxFirmware/emonTxV3/RFM/emonTxV3.4/emonTxV3_4_DiscreteSampling/compiled/emonTxV3_RFM69CW_latest_868.hex
 
