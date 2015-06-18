ccc135ad37ce0d90937cbb5c9a2174aa4de47f42
Glyn Hudson <glyn.hudson@gmail.com>
2015-06-18 14:54:18 +0100

    Update rfm12b433.sh

--- emontx34/rfm12b433.sh
+++ emontx34/rfm12b433.sh
@@ -1,3 +1,2 @@
 echo "starting avrdude upload V3.4 RFM12B 433Mhz"
-avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTxFirmware/emonTxV3/RFM/emonTxV3.4/emonTxV3_4_DiscreteSampling/V1_6_emonTxV3_RFM12B_DiscreteSampling.cpp_433.hex
-
+avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTxFirmware/emonTxV3/RFM/emonTxV3.4/emonTxV3_4_DiscreteSampling/compiled/emonTxV3_RFM12B_latest_433.hex
