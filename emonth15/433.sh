21e06a882b2444fc6fc854e83bd9bfa96cbbb982
Glyn Hudson <glyn.hudson@gmail.com>
2015-08-10 20:33:58 +0100

    update emonth upload script

--- emonth15/433.sh
+++ emonth15/433.sh
@@ -1,6 +1,4 @@
-while :
-	do
+
 		echo "starting avrdude upload emonTH V1.5 433Mhz"
 		avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTH/emonTH_DHT22_DS18B20_RFM69CW/emonTH_DHT22_DS18B20_RFM69CW.cpp.hex
-		sleep 5
-done
+
--- emonth15/868.sh
+++ emonth15/868.sh
@@ -1,6 +1,3 @@
-while :
-	do
+
 		echo "starting avrdude upload emonTH V1.5 433Mhz"
 		avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTH/emonTH_DHT22_DS18B20_RFM69CW/emonTH_DHT22_DS18B20_RFM69CW.cpp_868.hex
-		sleep 5
-done
