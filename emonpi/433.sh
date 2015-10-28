bbd366642367e47ae88b3ca467217149fcb20f81
root <root@moelwyn>
2015-10-28 15:36:53 +0000

    update

--- emonpi/433.sh
+++ emonpi/433.sh
@@ -1,6 +1,4 @@
-while :
-	do
+
 		echo "starting avrdude upload emonPi Serial upload"
 		avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonpi/Atmega328/emonPi_RFM69CW_RF12Demo_DiscreteSampling/compiled/emonPi_latest.hex
-		sleep 5
-done
+
