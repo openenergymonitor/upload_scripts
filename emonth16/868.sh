4a27d612ebc969d420f162d46e7702cbbb79e13b
oem <oem@moelwyn>
2015-11-03 12:32:07 +0000

    update

--- emonth16/868.sh
+++
@@ -1,3 +0,0 @@
-
-		echo "starting avrdude upload emonTH V1.5 868Mhz"
-		avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTH/emonTH_DHT22_DS18B20_RFM69CW/emonTH_DHT22_DS18B20_RFM69CW.cpp_868.hex
