41628fe2a2822c88dddf15f0372daab55ebc013e
Glyn Hudson <glyn.hudson@gmail.com>
2015-11-23 16:49:23 +0000

    Update all.py

--- automated/upload_and_test_scripts/all.py
+++ automated/upload_and_test_scripts/all.py
@@ -1,115 +1,68 @@
-# Script for RaspberryPi with RFM12Pi 
-# Automated upload and test 
-# Upload code to emonTx via ISP and check for RF data received 
+#!/usr/bin/env python
 
-# By Glyn Hudson 20/04/2015
+# Automated upload and test
+# Upload code to ATmega328 via ISP / Serial and check for RF data received
+
+# By Glyn Hudson
 # Part of the openenergymonitor.org project
 
 
 import serial, sys, string, commands, time, subprocess
 from subprocess import Popen, PIPE, STDOUT
 
-print' '
-print 'OpenEnergyMonitor all unit Upload & test 20/04/15'
-print ' ' 
-print 'Select Upload:'
+###################################################### DEFENITIONS ###################################################################
+rx_device = False # Is Rx device present?
+rx_port = '/dev/ttyUSB1'
+rx_baud = 38400
+rx_group = '210g'
+rx_frequency = '4b'
 
-print 'setting Rx module to receive on 433Mhz and 210 network group....'
-ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)
-ser.write("210g")
-time.sleep(1)
-ser.write("4b")
-ser.close()
+firmware_base_path = '/home/oem/firmware/'
+emontx_path = 'emonTxFirmware/emonTxV3/RFM/emonTxV3.4/emonTxV3_4_DiscreteSampling/compiled/'
+emonth_path = 'emonTH/emonTH_DHT22_DS18B20_RFM69CW_Pulse/compiled/'
+emonpi_path = 'emonpi//Atmega328/emonPi_RFM69CW_RF12Demo_DiscreteSampling/compiled/'
 
-while(1):
-	print ' ' 
-	print 'Enter >' 
-	print '(x) for emonTx'
-	print '(h) for emonTH'
-	print '(i) for emonPi'
-	print '(r) for RFM69Pi'
-	print '(e) to EXIT and shutdown'
-	nb = raw_input('> ')
-        print(nb)
+device = 'ATMEGA328P'
+upload_port = '/dev/ttyUSB0'
+upload_baud = '115200'
+new_serial_baud = '38400'
+new_serial_baud = '9600'
 
-	if nb=='x': 
-		print 'emonTx V3.4 RFM69CW 433Mhz'
-		print 'Attempting RFM69CW  433Mhz emonTx firmware upload via ISP....'
-		cmd = 'sudo avrdude -V -u -p atmega328p -c avrispmkII -P usb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m -U flash:w:/home/pi/emonTxFirmware/emonTxV3/RFM/emonTxV3.4/emonTxV3_4_DiscreteSampling/compiled/emonTxV3_RFM69CW_latest_433_bootloader.hex:i  -Ulock:w:0x0F:m'
-		subprocess.call(cmd, shell=True)
-                time.sleep(1)
-		ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)
-		linestr = ser.readline()
-		print linestr
-		#print len(linestr)
-		if (len(linestr)>0):
-			if (int(linestr[3] + linestr[4])==10) | (int(linestr[3] + linestr[4])==8): 
-				print 'PASS!...RF RECEIVE SUCCESS from emonTx'
-			else:
-				print 'FAIL...RF received but not from the emonTx'
-		else: 
-			print 'FAIL...no RF received'
-		ser.close()
+isp_upload_default = '-V -u -p atmega328p -c avrispmkII -P usb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m -U flash:w:'
+serial_upload_default = '-u -c arduino -p' + device + '-P' + upload_port  + '-b' + upload_baud + '-U flash:w:'
 
-	if nb=='h': 
-		print 'emonTH RFM69CW 433Mhz'
-		print 'RFM69CW 433Mhz emonTH firmware upload via ISP....'
-		cmd = 'sudo avrdude -V -u -p atmega328p -c avrispmkII -P usb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m -U flash:w:/home/pi/emonTH/emonTH_DHT22_DS18B20_RFM69CW_Pulse/compiled/emonTH_latest_Bootloader.hex:i  -Ulock:w:0x0F:m'
-		subprocess.call(cmd, shell=True)
-                time.sleep(1)
-		ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)
-		linestr = ser.readline()
-		print linestr
-		if (len(linestr)>0):
-			if ((int(linestr[3] + linestr[4])==19) | (int(linestr[3] + linestr[4])==23)): 
-				print 'PASS!...RF RECEIVE SUCCESS from emonTH RFM69CW on node 19 or 23'
-			else:
-				print 'FAIL...RF received but not from the emonTH on node 19 or 23 (Check DIP switch setting?)'
-		else: 
-			print 'FAIL...no RF received'
-		ser.close()
+#####################################################################################################################################
 
-	if nb=='r': 
-		print 'RFM69Pi 433Mhz'
-		print 'RFM69Pi firmware upload via ISP....'
-		cmd = 'sudo avrdude -V -u -p atmega328p -c avrispmkII -P usb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xE2:m -U flash:w:/home/pi/RFM2Pi/firmware/RFM69CW_RF_Demo_ATmega328/Optiboot328_8mhz_RFM69CW_RF12_Demo_ATmega328.cpp.hex:i'
-		subprocess.call(cmd, shell=True)
-                time.sleep(1)
-		print 'Check JeeLink transmitter connected > Flashing RED LED on the RFM69Pi = SUCCESS?'
+print 'OpenEnergyMonitor Upload & Test'
+print ' '
+print "Select Upload"
 
-	if nb=='i': 
-		print 'emonPi RFM69CW 433Mhz'
-		print 'RFM69CW 433Mhz emonPi firmware upload via ISP....'
-		cmd = 'sudo avrdude -V -u -p atmega328p -c avrispmkII -P usb -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xDE:m -Ulfuse:w:0xFF:m -U flash:w:/home/pi/emonpi/Atmega328/emonPi_RFM69CW_RF12Demo_DiscreteSampling/compiled/emonPi_latest_bootloader.hex:i'
-
-		subprocess.call(cmd, shell=True)
-                time.sleep(1)
-		ser = serial.Serial('/dev/ttyAMA0', 38400, timeout=1)
-		linestr = ser.readline()
-		print linestr
-		if (len(linestr)>0):
-			if (int(linestr[2] + linestr[3])==5): 
-				print 'PASS!...RF RECEIVE SUCCESS from emonPi'
-			else:
-				print 'FAIL...RF received but not from the emonPi on node 5'
-		else: 
-			print 'FAIL...no RF received'
-		ser.close()
-
-	if nb=='e':
-		print 'END'
-		print 'Raspberry Pi Shutdown NOW!....'
-		time.sleep(2)
-		cmd = 'sudo halt'
-		subprocess.call(cmd, shell=True)
-        	sys.exit
-
-
-
-
-
-
-
-	#if ((nb!=8) and (nb!=4)):
-	#	print 'Invalid selection, please restart script and select b or w'
-
+while(True):
+    print "MENU: "
+    print 'Make selection then hit Enter >'
+    print "(1) for emonTx (Firmware V2.3)"
+    print "(2) for emonTH (Firmware V2.6)"
+    print "(3) for emonPi (Firmware V2.0)"
+    print "(4) for RFM69Pi (Firmware V1.3)"
+    print ' '
+    print "(5) to view serial window"
+    print "(e) to EXIT"
+    nb = raw_input('> ')
+    if nb=='1':
+	    print "(1) for emonTx RFM69CW 433Mhz"
+	    print "(2) for emonTx RFM69CW 868Mhz"
+	    nb2 = raw_input('> ')
+	    if nb2=='1':
+		    while True:
+			    print "emonTx RFM69CW 433Mhz"
+			    cmd = serial_upload_default + firmware_base_path + emontx_path + 'emonTxV3_RFM69CW_latest_433.hex'
+			    subprocess.call(cmd, shell=True)
+			    time.sleep(1)
+          	    ser = serial.Serial(upload_port, new_serial_baud, timeout=2)
+          	    serial = ser.readline()
+          	    print serial
+          	    choice = raw_input('Hit Enter to upload again or any other key then Enter to return to menu>')
+          	    if choice == '':
+          		    print 'upload again...'
+          		if choice != ''
+          		    break
