962cf516215d08831cf0ea934028d28c06894952
Glyn Hudson <glynhudson@users.noreply.github.com>
2016-08-18 12:27:35 +0100

    update ds18b2 in dht22 socket location

--- emonth15/433_ds18b20_in_dht22.sh
+++ emonth15/433_ds18b20_in_dht22.sh
@@ -1,4 +1,4 @@
 
 		echo "starting avrdude upload emonTH V1.5 433Mhz DS18B20 in DHT22 Socket"
-		avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTH/emonTH_DHT22_DS18B20_RFM69CW/emonTH_DS18B20_in_DHT22_socket/emonTH_DS18B20_in_DHT22_socket.cpp.hex
+		avrdude  -u -c arduino -p ATMEGA328P -P /dev/ttyUSB0 -b 115200 -U flash:w:/home/oem/firmware/emonTH/emonTH_DHT22_DS18B20_RFM69CW_Pulse/compiled/emonTH_DS18B20_in_DHT22_socket.cpp.hex
 
